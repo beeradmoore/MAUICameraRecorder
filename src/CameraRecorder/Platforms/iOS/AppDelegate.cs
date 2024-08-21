ab4e2dc16d0bf3b2d8577054c0e79b06cc0b2d0b
Brad <brad@4pi.com.au>
2024-08-22 08:57:22 +1000

    Initial commit

---
+++ src/CameraRecorder/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CameraRecorder;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
